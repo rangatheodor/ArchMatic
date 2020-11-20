93578a97473bec09ba475bcbad75e35aa12af253
Dodo <rangatheodor@gmail.com>
2020-11-20 09:48:29 +0200

    Update preinstall.sh
    Commented out mirrorlist configuration.

--- preinstall.sh
+++ preinstall.sh
@@ -7,13 +7,13 @@
 #  Arch Linux Post Install Setup and Config
 #-------------------------------------------------------------------------
 
-echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - US Only"
-echo "-------------------------------------------------"
-timedatectl set-ntp true
-pacman -S --noconfirm pacman-contrib
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+#echo "-------------------------------------------------"
+#echo "Setting up mirrors for optimal download - US Only"
+#echo "-------------------------------------------------"
+#timedatectl set-ntp true
+#pacman -S --noconfirm pacman-contrib
+#mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+#curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 
 
